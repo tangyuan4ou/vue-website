c11a58658e09d40982029b609726e0281116ce26
tangyuan4ou <tangyuan_youyou@163.com>
2017-12-15 17:25:51 +0800

    add window.onresize function

--- src/page/foo/index.vue
+++ src/page/foo/index.vue
@@ -23,16 +23,14 @@
         div.swiper-pagination(solt="pagination")
 </template>
 <script>
-    import Vue from 'vue'
     import 'swiper/dist/css/swiper.css'
     import { swiper, swiperSlide } from 'vue-awesome-swiper'
-    import { mapState, mapActions, mapGetters } from 'vuex'
 
     import VueNav from '~/components/nav'
     import BannerWrap from '~/components/index/banner'
-    import Construction from '~/components/index/construction'
     import Character from '~/components/index/character'
     import ProTrait from '~/components/index/proTrait'
+    import Construction from '~/components/index/construction'
     import Partner from '~/components/index/partner'
     import VueFooter from '~/components/footer'
     
@@ -63,65 +61,62 @@
             document.body.classList.add('bodyHidden')
         },
         mounted() {
-            this.swiperOption.paginationBulletRender = (swiper, index, className) => {
-                let pDesc = ''
-                switch (index) {
-                case 0:
-                    pDesc = 'banner'
-                    break
-                case 1:
-                    pDesc = '产品功能'
-                    break
-                case 2:
-                    pDesc = '产品优势'
-                    break
-                case 3:
-                    pDesc = '敬请期待'
-                    break
-                case 4:
-                    pDesc = '合作伙伴'
-                    break
-                default:
-                    break
-                }
-                return `<span class="${className}"><span class="pDesc">${pDesc}</span></span>`
-            }
-
-            this.swiperOption.onSlideChangeStart = swiper => {
-                const activeIndex = swiper.activeIndex
-                switch (activeIndex) {
-                case 1:
-                    this.navStyle = '#000000'
-                    this.character = 'c_con_an'
-                    break
-                case 2:
-                    this.navStyle = '#000825'
-                    this.trait = 'tr_con_an'
-                    break
-                case 3:
-                    this.navStyle = '#000000'
-                    this.constreuce = 'con_con_an'
-                    break
-                case 4:
-                    this.navStyle = '#172250'
-                    this.partner = 'p_con_an'
-                    break
-                default:
-                    this.navStyle = '#161823'
-                    break
-                }
-            }
+            this.init()
         },
         methods: {
+            init() {
+                window.onresize = () => {
+                    this.swiperOption.height = window.innerHeight > 550 ? window.innerHeight - 80 : 550
+                }
+                this.paginationBulletRender()
+                this.onSlideChangeStart()
+            },
+            paginationBulletRender() {
+                this.swiperOption.paginationBulletRender = (swiper, index, className) => {
+                    let pDesc = ''
+                    switch (index) {
+                    case 0: pDesc = 'banner'
+                        break
+                    case 1: pDesc = '产品功能'
+                        break
+                    case 2: pDesc = '产品优势'
+                        break
+                    case 3: pDesc = '敬请期待'
+                        break
+                    case 4: pDesc = '合作伙伴'
+                        break
+                    default:
+                        break
+                    }
+                    return `<span class="${className}"><span class="pDesc">${pDesc}</span></span>`
+                }
+            },
+            onSlideChangeStart() {
+                this.swiperOption.onSlideChangeStart = swiper => {
+                    const activeIndex = swiper.activeIndex
+                    switch (activeIndex) {
+                    case 1: this.navStyle = '#000000'; this.character = 'c_con_an'
+                        break
+                    case 2: this.navStyle = '#000825'; this.trait = 'tr_con_an'
+                        break
+                    case 3: this.navStyle = '#000000'; this.constreuce = 'con_con_an'
+                        break
+                    case 4: this.navStyle = '#172250'; this.partner = 'p_con_an'
+                        break
+                    default: this.navStyle = '#161823'
+                        break
+                    }
+                }
+            }
         },
         components: {
             swiper,
             swiperSlide,
             VueNav,
             BannerWrap,
-            Construction,
             Character,
             ProTrait,
+            Construction,
             Partner,
             VueFooter
         }
